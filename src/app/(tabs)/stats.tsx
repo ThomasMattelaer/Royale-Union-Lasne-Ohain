0bbb52a6c7b90d40c8038038ffa52424fb47275f
Thomas Mattelaer <thomas@MacBook-Air-de-Thomas.local>
2025-11-12 16:36:10 +0100

    init project rulo

---
+++ src/app/(tabs)/stats.tsx
@@ -0,0 +1,10 @@
+import { Text, View} from "react-native";
+import { Link  } from "expo-router";
+
+export default function StatsScreen(){
+  return (
+    <View>
+      <Text>News of the RULO</Text>
+    </View>
+  )
+} 
